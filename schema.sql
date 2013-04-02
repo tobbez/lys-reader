94a3e6ab094313589e370d8923019d8a343a8e3e
Torbjörn Lönnemark <tobbez@ryara.net>
2013-04-02 17:23:41 +0200

    Add initial draft of database schema

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE lysr_user (
+	id BIGSERIAL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE,
+	password VARCHAR(60)
+);
+
+CREATE TABLE lysr_user_settings (
+	id BIGSERIAL PRIMARY KEY,
+	user BIGINT NOT NULL REFERENCES lysr_user (id) ON DELETE CASCADE
+);
+
+CREATE TABLE lysr_feed (
+	id BIGSERIAL PRIMARY KEY,
+	url DATALINK UNIQUE
+);
+
+CREATE TABLE lysr_user_feed (
+	id BIGSERIAL PRIMARY KEY,
+	user BIGINT NOT NULL REFERENCES lysr_user (id) ON DELETE CASCADE,
+	feed BIGINT NOT NULL REFERENCES lysr_feed (id) ON DELETE CASCADE
+);
+
+CREATE TABLE lysr_feed_entry (
+	id BIGSERIAL PRIMARY KEY,
+	feed BIGINT NOT NULL REFERENCES lysr_feed (id) ON DELETE CASCADE,
+	-- feed contents
+);
+
+CREATE TABLE lysr_feed_entry_status (
+	id BIGSERIAL PRIMARY KEY,
+	user BIGINT NOT NULL REFERENCES lysr_user (id) ON DELETE CASCADE,
+	feed_entry BIGINT NOT NULL REFERENCES lysr_feed_entry (id) ON DELETE CASCADE,
+	read BOOLEAN,
+	starred BOOLEAN
+);
+
+
+-- tags
+CREATE TABLE lysr_tag (
+	id BIGSERIAL PRIMARY KEY,
+	name TEXT
+);
+
+CREATE TABLE lysr_tag_assoc (
+	id BIGSERIAL PRIMARY KEY,
+	tag BIGINT REFERENCES lysr_tag (id) ON DELETE CASCADE,
+	feed BIGINT REFENECES lysr_user_feed (id) ON DELETE CASCADE
+);
+-- /tags
