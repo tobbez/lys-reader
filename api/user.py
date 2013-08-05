6ff58b1791398fcb124fa049757eb0a8707d283d
Joakim Braaf <baafen@baafen.se>
2013-08-05 22:33:06 +0200

    Add base for user api

---
+++ api/user.py
@@ -0,0 +1,40 @@
+from common.database import DB
+
+def register_user(email, password):
+
+    if not is_registered(email):
+        con = DB.connection
+        cur = con.cursor()
+
+        cur.execute('INSERT INTO lysr_user (email, password) VALUES (%s, %s)', (email, password))
+        con.commit()
+
+        return True
+
+    return False
+
+def login_user(email, password):
+
+    if is_registered(email):
+        con = DB.connection
+        cur = con.cursor()
+
+        cur.execute('SELECT id FROM lysr_user WHERE email = %s and password = %s', (email, password))
+        con.commit()
+
+        if cur.rowcount is 1:
+            return True
+
+        return False
+
+def is_registered(email):
+    con = DB.connection
+    cur = con.cursor()
+
+    cur.execute('SELECT id FROM lysr_user WHERE email = %s', (email,))
+    con.commit()
+
+    if cur.rowcount is 0:
+        return False
+
+    return True
---
+++ api/userview.py
@@ -0,0 +1,31 @@
+from flask import abort, request, jsonify, make_response, session
+from api import app
+from api.user import *
+
+@app.route('/api/user/register', methods = ['POST'])
+def api_user_register():
+    if 'email' in request.json and 'password' in request.json:
+        if register_user(request.json['email'], request.json['password']):
+            return make_response(jsonify({ 'status':'OK', 'message':'User account created'}), 200)
+        else:
+            return make_response(jsonify({ 'status':'FAIL', 'message':'User account not created'}), 200)
+
+    return make_response(jsonify({ 'status':'BAD REQUEST', 'message':'Missing parameters'}), 400)
+
+@app.route('/api/user/login', methods = ['POST'])
+def api_user_login():
+    if 'email' in request.json and 'password' in request.json:
+        if login_user(request.json['email'], request.json['password']):
+            session['email'] = request.json['email']
+            return make_response(jsonify({ 'status':'OK', 'message':'User logged in successfully'}), 200)
+        else:
+            return make_response(jsonify({ 'status':'FAIL', 'message':'Email and password combination did not match'}), 200)
+    return make_response(jsonify({ 'status':'BAD REQUEST', 'message':'Missing parameters'}), 400)
+
+@app.route('/api/user/logout')
+def api_user_logout():
+    if 'email' in session:
+        session.pop('username', None)
+        return make_response(jsonify({ 'status':'OK', 'message':'User logged out successfully'}), 200)
+
+    return make_response(jsonify({ 'status':'BAD REQUEST', 'message':'User not logged in'}), 403)
