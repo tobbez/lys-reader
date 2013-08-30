78977a0f976615e76db477b0ab7b35193b34d189
Joakim Braaf <baafen@lysator.liu.se>
2013-08-30 13:29:20 +0200

    Change so that kvsession (server side sessions) is used instead of flask default

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from simplekv.memory import DictStore
+from flaskext.kvsession import KVSessionExtension
 
+# Use DictStore until the code is ready for production
+store = DictStore()
 app = Flask(__name__)
 app.secret_key = ''
+
+KVSessionExtension(store, app)
 
 import api.userview
