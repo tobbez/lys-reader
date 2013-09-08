067bbf7c754e22398f567f9c3c9b350adce75839
Baafen <baafen@baafen.se>
2013-09-08 21:46:46 +0200

    Add underscore to "private" members

--- backend/feedhandler.py
+++ backend/feedhandler.py
@@ -8,14 +8,14 @@ class FeedHandler():
     def __init__(self):
         logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
         config   = get_config()
-        self.DB  = Database(config)
-        self.con = self.DB.get_connection()
+        self._DB  = Database(config)
+        self._con = self._DB.get_connection()
 
     def update_feed(self, feed_id, feed_url=None):
         if feed_url == None:
-            cur = self.con.cursor()
+            cur = self._con.cursor()
             cur.execute('SELECT url FROM lysr_feed WHERE id=%s', (feed_id,))
-            self.con.commit()
+            self._con.commit()
             feed_url = cur.fetchone()[0]
 
         logging.info('Updating feed {}: {}'.format(feed_id, feed_url))
@@ -25,18 +25,18 @@ class FeedHandler():
         new_entries = 0
         if feed.status is 200:
             try:
-                cur = self.con.cursor()
+                cur = self._con.cursor()
     
                 for entry in feed.entries:
                     # Bad HTML is removed by default
                     cur.execute('SELECT id FROM lysr_feed_entry WHERE feed = %s AND guid = %s', (feed_id, entry.link))
-                    self.con.commit()
+                    self._con.commit()
                
                     if cur.rowcount is 0:
                         new_entries += 1
                         cur.execute('INSERT INTO lysr_feed_entry (feed, guid, content, title) VALUES (%s, %s, %s, %s)',
                                 (feed_id, entry.link, entry.description, entry.title))
-                        self.con.commit()
+                        self._con.commit()
 
                 
                 logging.info('Fetched feed {}, {} new entries found'.format(feed_id, new_entries))
@@ -46,22 +46,22 @@ class FeedHandler():
         else:
             logging.info('Failed to fetch feed {}, status {}'.format(feed_id, feed.status))
 
-        cur = self.con.cursor()
+        cur = self._con.cursor()
 
         cur.execute('UPDATE lysr_feed SET last_check=NOW() WHERE id=%s', (feed_id,))
-        self.con.commit()
+        self._con.commit()
 
         if new_entries:
             cur.execute('UPDATE lysr_feed SET last_update=NOW() WHERE id=%s', (feed_id,))
         else:
             cur.execute('''UPDATE lysr_feed SET update_interval=LEAST(1.5*update_interval, '7d'::interval) WHERE id=%s''', (feed_id,))
-        self.con.commit()
+        self._con.commit()
 
 
     def update_feeds(self):
-        cur = self.con.cursor()
+        cur = self._con.cursor()
         cur.execute('SELECT id, url FROM lysr_feed WHERE NOW() > last_check + update_interval')
-        self.con.commit()
+        self._con.commit()
 
         for feed in cur:
             self.update_feed(*feed)
