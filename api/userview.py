dffcfa42fbf4f200a22b739a0cd24f36317b054c
Baafen <baafen@baafen.se>
2013-09-28 11:41:52 +0200

    Fix so that /api/login/ follow the specified api documentation.

--- api/userview.py
+++ api/userview.py
@@ -28,18 +28,26 @@ def api_user_signup():
 @require_csrf_token
 @app.route('/api/login/', methods = ['POST'])
 def api_user_login():
+    generate_csrf_token(session)
+    status = {}
+    httpcode = 200
+
     if 'email' in request.json and 'password' in request.json:
         id = check_user_credentials(request.json['email'], request.json['password'])
         if id is not None:
-            session = app.open_session(request)
             session['id'] = id
             session['loggedin'] = True
-            response = make_response(jsonify({ 'status': 'OK', 'message': 'User logged in successfully'}), 200)
-            app.save_session(session, response)
+            status['code'] = 0
+            status['message'] = 'Success'
         else:
-             response = make_response(jsonify({ 'status': 'FAIL', 'message': 'Email and password combination did not match'}), 200)
-        return response
-    return make_response(jsonify({ 'status': 'BAD REQUEST', 'message': 'Missing parameters'}), 400)
+            status['code'] = 4
+            status['message'] = 'Email and password combination did not match'
+    else:
+        status['code'] = 2
+        status['message'] = 'Missing paramter(s)'
+        httpcode = 400
+
+    return make_response(jsonify({ 'csrf_token': session['csrf'], 'status': status }), httpcode)
 
 @require_csrf_token
 @require_authentication
