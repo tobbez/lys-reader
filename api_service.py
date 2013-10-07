e4a54d3e52c74f5762a29b5796f9cc7a824d4160
Baafen <baafen@baafen.se>
2013-10-07 20:54:02 +0200

    Restructure code

--- api_service.py
+++ api_service.py
@@ -3,12 +3,11 @@ from simplekv.fs import FilesystemStore
 from flaskext.kvsession import KVSessionExtension
 
 store = FilesystemStore('session')
+KVSessionExtension(store, app)
 
 app.config.from_pyfile('../config.defaults.py')
 app.config.from_pyfile('../config.py')
 app.secret_key = app.config['SECRET_KEY']
-
-KVSessionExtension(store, app)
 
 if __name__ == '__main__':
     app.run(debug = True)
